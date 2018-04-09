a71c7b2f791e5b51be466ae51f4d4122563a1606
Raoul Baron <raoul.baron@student.hpi.de>
2018-04-09 15:58:02 +0200

    add youtube support for video plugin

--- src/plugins/VideoPlugin/VideoPlugin.jsx
+++ src/plugins/VideoPlugin/VideoPlugin.jsx
@@ -1,75 +1,70 @@
-import React, { Component } from 'react'
-import { connect } from "react-redux";
-import PropTypes from 'prop-types';
-import isEqual from "lodash.isequal";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./styles.scss";
+
+const youtubeR = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#&?]*).*/m;
+const vimeoR = /(http|https)?:\/\/(www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^/]*)\/videos\/|)(\d+)(?:|\/\?)/;
 
 class VideoPlugin extends Component {
-
     constructor(props) {
         super(props);
 
         this.state = {
-            inputURL: '',
-            embedURL: ''
-        }
-    }
-
-    shouldComponentUpdate({ editable }, nextState) {
-        return editable && !isEqual(this.state, nextState);
+            embedURL: ""
+        };
     }
 
     componentDidMount() {
-
         this.setState({
-            ...this.props.content,
+            ...this.props.content
         });
-
     }
 
     render() {
-
-        const { editable, saveContent } = this.props;
+        const { isEditable, saveContent } = this.props;
 
         return (
-            <React.Fragment>
-                <div>unstyled</div>
-                <div>{`Editable: ${editable}`}</div>
-                <input
-                    autoFocus={true}
-                    type="url"
-                    name="url"
-                    value={this.state.inputUrl}
-                    onChange={(e) => this.handleChange(e)} />
-                <iframe
-                    width="420"
-                    height="315"
-                    src={this.state.embedURL}
-                    frameborder="0"
-                    allowfullscreen>
-                </iframe>
-            </React.Fragment>
-        )
+            <div className={styles.video_wrapper}>
+                <div className={styles.iframe_wrapper}>
+                    <iframe
+                        src={this.state.embedURL}
+                        frameBorder="0"
+                        allowFullScreen
+                    />
+                </div>
+                <div>
+                    <input
+                        className={styles.video_input}
+                        autoFocus={true}
+                        type="url"
+                        name="url"
+                        onInput={e => this.handleChange(e)}
+                        disabled={!isEditable}
+                        placeholder="Gib eine Youtube URL ein"
+                    />
+                </div>
+            </div>
+        );
     }
 
     handleChange(e) {
+        const [,, id] = youtubeR.exec(e.target.value);
 
-        this.setState({
-            [e.target.name]: e.target.value,
-        }, () => {
-            var arr = this.state.inputURL.match(/^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/gm);
-            var id = arr[0];
-
-            this.state.embedURL = 'http://www.youtube.com/embed/' + id;
-
-            this.props.saveContent(this.state);
-        });
+        this.setState(
+            {
+                embedURL: `http://www.youtube.com/embed/${id}`
+            },
+            () => this.props.saveContent(this.state)
+        );
     }
 
-    static propTypes = { // fuer Linting
-        editable: PropTypes.bool,
-        content : PropTypes.object,
-        saveContent: PropTypes.func,
+    static propTypes = {
+        // fuer Linting
+        isEditable: PropTypes.bool,
+        content: PropTypes.object,
+        saveContent: PropTypes.func
     };
 }
 
-export default VideoPlugin
+export default VideoPlugin;
