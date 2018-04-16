52ecf0a698ccce79b19f29a750505e6c2aa2a25c
Raoul Baron <raoul.baron@student.hpi.de>
2018-04-16 15:15:00 +0200

    make content saveable

--- src/plugins/Text/TextPlugin.jsx
+++ src/plugins/Text/TextPlugin.jsx
@@ -1,36 +1,49 @@
-import ReactQuill from 'react-quill'
+import ReactQuill from "react-quill";
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class TextPlugin extends Component {
     constructor(props) {
-        super(props)
-        this.state = { text: '' } // You can also pass a Quill Delta here
-        this.handleChange = this.handleChange.bind(this)
+        super(props);
+        this.state = { text: "" }; // You can also pass a Quill Delta here
+    }
+
+    componentDidMount() {
+        this.setState({
+            ...this.props.content
+        });
     }
 
     handleChange(value) {
-        this.setState({ text: value })
+        this.setState({ text: value }, () =>
+            this.props.saveContent(this.state)
+        );
+    }
+
+    shouldComponentUpdate() {
+        return true;
     }
 
     render() {
-        const { isEditable/*, saveContent*/ } = this.props;
+        const { isEditable } = this.props;
 
-        if(isEditable) {
+        if (isEditable) {
             return (
-                <ReactQuill value={this.state.text}
-                    onChange={this.handleChange} />
-            )
+                <ReactQuill
+                    value={this.state.text}
+                    onChange={e => this.handleChange(e)}
+                />
+            );
         }
-        return (
-            <div dangerouslySetInnerHTML={{ __html: this.state.text }} />
-        )
+        return <div dangerouslySetInnerHTML={{ __html: this.state.text }} />;
     }
 }
 
 TextPlugin.propTypes = {
-    isEditable: PropTypes.bool.isRequired
-}
+    isEditable: PropTypes.bool.isRequired,
+    content: PropTypes.object,
+    saveContent: PropTypes.func.isRequired
+};
 
-export default TextPlugin
+export default TextPlugin;
