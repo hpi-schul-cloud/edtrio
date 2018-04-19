db219b170489dd9fb19d4da0cafeafc08c7e75a7
Florian <FWirtz@users.noreply.github.com>
2018-04-19 11:16:47 +0200

    :sparkles: Add plugin: Image
    Remove /src/plugins/Image from .gitignore
    Add src/plugins/index.js to .gitignore
    
    Fix bug breaking scrolling
    
    Fixes #25

---
+++ src/plugins/Image/ImagePlugin.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./styles.scss";
+
+
+class ImagePlugin extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            embedURL: ""
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            ...this.props.content
+        });
+    }
+
+    render() {
+        const { isEditable } = this.props;
+        const { embedURL } = this.state;
+
+        return (
+            <>
+                <img src={embedURL}/>
+                <input
+                    id={styles.imageUrl}
+                    autoFocus={true}
+                    type="url"
+                    name="url"
+                    onInput={e => this.handleChange(e)}
+                    disabled={!isEditable}
+                    placeholder="Bild URL eingeben"
+                />
+            </>
+        );
+    }
+
+    handleChange(e) {
+        this.setState(
+            {
+                embedURL: e.target.value
+            },
+            () => this.props.saveContent(this.state)
+        );
+    }
+
+    static propTypes = {
+        isEditable: PropTypes.bool,
+        content: PropTypes.object,
+        saveContent: PropTypes.func
+    };
+}
+
+export default ImagePlugin;
