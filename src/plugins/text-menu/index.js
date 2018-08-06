eee05b3fc01d9332b1ab73e963571bfc415fea63
FWirtz <florian.wirtz08@gmail.com>
2018-08-06 21:06:34 +0200

    :construction: Add wip flying menu (not working atm)

---
+++ src/plugins/text-menu/index.js
@@ -0,0 +1,27 @@
+import './style.css'
+
+import React from 'react'
+
+
+export default function TextMenu(options) {
+    return {
+        changes: {},
+        helpers: {},
+        components: {},
+        plugins: [],
+    }
+}
+
+const renderMarkButton = (type, icon) => {
+    // const { value } = this.props
+}
+
+function Menu(props) {
+    // lalalala
+
+    return (
+        <div className="text-menu">
+            { props.children }
+        </div>
+    )
+}
