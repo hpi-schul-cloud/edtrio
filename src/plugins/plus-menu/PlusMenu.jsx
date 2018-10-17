deed25ecb19af56208e32bc64935e8a4d680a0fb
FWirtz <florian.wirtz08@gmail.com>
2018-10-17 11:04:59 +0100

    :construction: Start work on Uppy translation
    Also see https://uppy.io/docs/dashboard/#locale

--- src/plugins/plus-menu/PlusMenu.jsx
+++ src/plugins/plus-menu/PlusMenu.jsx
@@ -63,9 +63,76 @@ class PlusMenu extends Component {
         )}
         <DashboardModal
           uppy={this.uppy}
+          locale={{
+            strings: {
+              // When `inline: false`, used as the screen reader label for the button that closes the modal.
+              closeModal: 'Fenster schließen',
+              // Used as the header for import panels, e.g., "Import from Google Drive".
+              importFrom: 'Von %{name} importieren',
+              // When `inline: false`, used as the screen reader label for the dashboard modal.
+              dashboardWindowTitle: 'Uppy Dashboard Window (ESC zum schließen)',
+              // When `inline: true`, used as the screen reader label for the dashboard area.
+              dashboardTitle: 'Uppy Dashboard',
+              // Shown in the Informer when a link to a file was copied to the clipboard.
+              copyLinkToClipboardSuccess: 'Link in Zwischenablage kopiert.',
+              // Used when a link cannot be copied automatically — the user has to select the text from the
+              // input element below this string.
+              copyLinkToClipboardFallback: 'Kopiere den Link unten',
+              // Used as the hover title and screen reader label for buttons that copy a file link.
+              copyLink: 'Link kopieren',
+              // Used as the hover title and screen reader label for file source icons, e.g., "File source: Dropbox".
+              fileSource: 'Quelle: %{name}',
+              // Used as the label for buttons that accept and close panels (remote providers or metadata editor)
+              done: 'Fertig',
+              // Used as the screen reader label for buttons that remove a file.
+              removeFile: 'Datei entfernen',
+              // Used as the screen reader label for buttons that open the metadata editor panel for a file.
+              editFile: 'Datei bearbeiten',
+              // Shown in the panel header for the metadata editor. Rendered as "Editing image.png".
+              editing: '%{file} wird bearbeitet',
+              // Text for a button shown on the file preview, used to edit file metadata
+              edit: 'Bearbeiten',
+              // Used as the screen reader label for the button that saves metadata edits and returns to the
+              // file list view.
+              finishEditingFile: 'Bearbeiten abschließen',
+              // Used as the label for the tab button that opens the system file selection dialog.
+              myDevice: 'Mein Gerät',
+              // Shown in the main dashboard area when no files have been selected, and one or more
+              // remote provider plugins are in use. %{browse} is replaced with a link that opens the system
+              // file selection dialog.
+              dropPasteImport:
+                'Ziehe Dateien hierhin, füge sie ein, %{browse} oder importiere sie aus',
+              // Shown in the main dashboard area when no files have been selected, and no provider
+              // plugins are in use. %{browse} is replaced with a link that opens the system
+              // file selection dialog.
+              dropPaste: 'Ziehe Dateien hierhin, füge sie ein oder %{browse}',
+              // This string is clickable and opens the system file selection dialog.
+              browse: 'durchsuche',
+              // Used as the hover text and screen reader label for file progress indicators when
+              // they have been fully uploaded.
+              uploadComplete: 'Hochladen fertig',
+              // Used as the hover text and screen reader label for the buttons to resume paused uploads.
+              resumeUpload: 'Hochladen fortsetzen',
+              // Used as the hover text and screen reader label for the buttons to pause uploads.
+              pauseUpload: 'Hochladen pausieren',
+              // Used as the hover text and screen reader label for the buttons to retry failed uploads.
+              retryUpload: 'Hochladen erneut versuchen',
+
+              // Used in a title, how many files are currently selected
+              xFilesSelected: {
+                0: '%{smart_count} Datei ausgewählt',
+                1: '%{smart_count} Dateien ausgewählt'
+              },
+
+              // @uppy/status-bar strings:
+              uploading: 'Uploading',
+              complete: 'Complete'
+              // ...etc
+            }
+          }}
           closeModalOnClickOutside
-          note="Images and video only, 2–3 files, up to 1 MB"
-          open={this.state.uppyOpen}
+          note="Bilder, maximal 1 MB"
+          open={true || this.state.uppyOpen}
           onRequestClose={this.handleCloseUppyModal}
         />
       </div>,
