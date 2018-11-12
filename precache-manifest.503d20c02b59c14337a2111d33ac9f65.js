3572dc5c54ce90fabeab479d1351cf3ec97bb4f8
FWirtz <florian.wirtz08@gmail.com>
2018-11-12 18:12:54 +0000

    Updates

---
+++ precache-manifest.503d20c02b59c14337a2111d33ac9f65.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/edtrio/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/edtrio/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/edtrio/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/edtrio/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "e50c7ce129d152e932ed",
+    "url": "/edtrio/static/js/runtime~main.e50c7ce1.js"
+  },
+  {
+    "revision": "e0b1eb66f172655145ec",
+    "url": "/edtrio/static/js/main.e0b1eb66.chunk.js"
+  },
+  {
+    "revision": "bf839270fc6946545bfe",
+    "url": "/edtrio/static/js/1.bf839270.chunk.js"
+  },
+  {
+    "revision": "e0b1eb66f172655145ec",
+    "url": "/edtrio/static/css/main.95aab6c6.chunk.css"
+  },
+  {
+    "revision": "bf839270fc6946545bfe",
+    "url": "/edtrio/static/css/1.41eaba9e.chunk.css"
+  },
+  {
+    "revision": "46a9d21035bb4f4a21141b5501a03392",
+    "url": "/edtrio/index.html"
+  }
+];
