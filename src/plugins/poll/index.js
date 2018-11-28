06f3aaab75e2f4749b18ea2cb7c2053378100651
lassesteffen <lasse.steffen@t-online.de>
2018-11-28 21:02:39 +0100

    Added chartist.js

--- src/plugins/poll/index.js
+++ src/plugins/poll/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { Text, Block } from "slate";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
+
+import "chartist/dist/chartist.css";
+import ChartistGraph from "react-chartist";
 
 export default function Poll(options) {
   return {
@@ -54,7 +55,7 @@ const onClickNewAnswerButton = (event, change, onChange, node) => {
 const appendNewAnswer = (change, node) => {
   const newAnswer = Block.create({
     type: "poll_answer",
-    nodes: [Text.create("Hier Antwortmöglichkeit eingeben...")],
+    nodes: [Text.create("")],
   });
   const lastIndex = node.nodes.count();
 
@@ -65,22 +66,55 @@ const appendNewAnswer = (change, node) => {
 
 const onClickDeleteAnswerButton = (event, change, onChange, node) => {
   event.preventDefault();
-  onChange(change.call(deleteAnswer, node));
+  onChange(change.call(deleteNode, node));
 };
 
-const deleteAnswer = (change, node) => {
+const onClickDeleteQuestionButton = (event, change, onChange, node) => {
+  event.preventDefault();
+  onChange(change.call(deleteNode, node));
+};
+
+const deleteNode = (change, node) => {
   return change.removeNodeByKey(node.key);
 };
 
 function PollNode(props) {
   const { children, node, editor, ...attributes } = props;
 
+  // Chartist graph
+  const labels = children.map(p => p.props.node.text);
+  labels.shift();
+  const series = labels.map((a, idx) => Math.floor(Math.random() * 10) + 1);
+
+  let data = {
+    labels: labels,
+    series: [series],
+  };
+
+  let options = {
+    high: Math.max(...series) + 1,
+    low: 0,
+  };
   return (
     <div>
       <ul className="collection with-header" {...attributes}>
         {children}
       </ul>
       <div className="right-align">
+        <button
+          className="btn-flat"
+          onClick={event =>
+            onClickDeleteQuestionButton(
+              event,
+              editor.value.change(),
+              editor.props.onChange,
+              node,
+            )
+          }
+        >
+          <i className="material-icons left">delete</i>
+          Frage löschen
+        </button>
         <button
           className="btn-flat"
           onClick={event =>
@@ -96,6 +130,7 @@ function PollNode(props) {
           Antwort hinzufügen
         </button>
       </div>
+      <ChartistGraph data={data} options={options} type="Bar" />
     </div>
   );
 }
@@ -104,7 +139,9 @@ function PollQuestionNode(props) {
   const { children, ...attributes } = props;
   return (
     <li className="collection-header" {...attributes}>
-      <h2>{children}</h2>
+      <div>
+        <h2>{children}</h2>
+      </div>
     </li>
   );
 }
@@ -114,14 +151,10 @@ function PollAnswerNode(props) {
 
   return (
     <li className="collection-item row" {...attributes}>
-      <label className="col s11">
-        <input
-          type="radio"
-          name={"answergroup" + parentKey}
-          className="with-gap"
-        />
+      <div className="col s11">
+        <input type="radio" name={"answergroup" + parentKey} />
         <span>{children}</span>
-      </label>
+      </div>
       <div className="col s1 right-align">
         <button
           className="btn-flat"
