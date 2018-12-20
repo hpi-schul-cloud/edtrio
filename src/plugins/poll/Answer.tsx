3eb0be4c30214468e984cdb43b4f63ed127c4cbc
lassesteffen <lasse.steffen@t-online.de>
2018-12-20 09:27:33 +0100

    refactor answer to class and clean up question code

--- src/plugins/poll/Answer.tsx
+++ src/plugins/poll/Answer.tsx
@@ -4,56 +4,73 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Radio from "@material-ui/core/Radio";
 import DeleteIcon from "@material-ui/icons/Delete";
-import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import RadioButtonCheckedIcon from "@material-ui/icons/RadioButtonChecked";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 import React from "react";
 import { Editor } from "slate";
 
+export default class PollAnswerNode extends React.Component<{
+  readOnly: boolean;
+  node: any;
+  editor: any;
+}> {
+  public render() {
+    const { children, node, editor, readOnly, ...attributes } = this.props;
 
-// 0 122 158
+    if (readOnly) {
+      return this.renderReadOnly(node.text);
+    } else {
+      return this.renderEditMode(children, node, editor);
+    }
+  }
 
-const deleteNode = (editor: Editor, node: any) => {
-  return editor.removeNodeByKey(node.key);
-};
+  private renderReadOnly(text: string) {
+    const percentage = Math.floor(Math.random() * 100);
+    const color = "#007A9E";
+    const background = `linear-gradient(to right, ${color} ${0}%, ${color} ${percentage}%, white ${percentage}%, white ${100 -
+      percentage}%)`;
 
-const onClickDeleteAnswerButton = (event: any, editor: Editor, node: any) => {
-  event.preventDefault();
-  deleteNode(editor, node);
-};
-
-export default function PollAnswerNode(props: any) {
-  const { children, node, editor, parentKey, readOnly, ...attributes } = props;
-  const percentage = Math.floor(Math.random() * 100);
-  const color = "#007A9E";
-  const background = `linear-gradient(to right, ${color} ${0}%, ${color} ${percentage}%, white ${percentage}%, white ${100 -
-    percentage}%)`;
-
-  if (readOnly) {
     return (
       <ListItem style={{ background }} button={true} divider={true}>
-        <Radio 
+        <Radio
           name="ok"
           icon={<RadioButtonUncheckedIcon fontSize="small" />}
-          checkedIcon={<RadioButtonCheckedIcon fontSize="small" />} 
+          checkedIcon={<RadioButtonCheckedIcon fontSize="small" />}
         />
-        <ListItemText primary={node.text} />
+        <ListItemText primary={text} />
       </ListItem>
     );
-  } else {
+  }
+
+  private renderEditMode(children: any, node: any, editor: any) {
     return (
       <ListItem divider={true}>
         <ListItemSecondaryAction>
-          <IconButton onClick={event => onClickDeleteAnswerButton(event, editor, node)}>
+          <IconButton
+            onClick={event =>
+              this.onClickDeleteAnswerButton(event, editor, node)
+            }
+          >
             <DeleteIcon fontSize="small" />
           </IconButton>
         </ListItemSecondaryAction>
-        <Radio 
-          name="ok" disabled={true} 
+        <Radio
+          name="ok"
+          disabled={true}
           icon={<RadioButtonUncheckedIcon fontSize="small" />}
-          checkedIcon={<RadioButtonCheckedIcon fontSize="small" />} 
+          checkedIcon={<RadioButtonCheckedIcon fontSize="small" />}
         />
         {children}
       </ListItem>
     );
+  }
+
+  private deleteNode(editor: Editor, node: any) {
+    return editor.removeNodeByKey(node.key);
+  }
+
+  private onClickDeleteAnswerButton(event: any, editor: Editor, node: any) {
+    event.preventDefault();
+    this.deleteNode(editor, node);
   }
 }
--- src/plugins/poll/Question.tsx
+++ src/plugins/poll/Question.tsx
@@ -10,20 +10,11 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import CloseIcon from "@material-ui/icons/Close";
 import React from "react";
 
-const onClickDeletePollButton = (event: any, editor: any, node: any) => {
-  event.preventDefault();
-  deleteNode(editor, node);
-};
-
-const deleteNode = (editor: any, node: any) => {
-  return editor.removeNodeByKey(node.key);
-};
-
 export default class PollQuestionNode extends React.Component<{
   readOnly: boolean;
 }> {
   public state = {
-    open: false,
+    isDialogOpen: false,
     children: null,
     editor: null,
     parent: null,
@@ -34,7 +25,7 @@ export default class PollQuestionNode extends React.Component<{
     super(props);
     const { children, editor, parent, readOnly, ...attributes } = props;
     this.state = {
-      open: false,
+      isDialogOpen: false,
       children,
       editor,
       parent,
@@ -44,49 +35,62 @@ export default class PollQuestionNode extends React.Component<{
 
   public render() {
     if (this.props.readOnly) {
-      return (
-        <ListItem divider={true} {...this.state.attributes}>
-          <h2>{this.props.children}</h2>
-        </ListItem>
-      );
+      return this.renderReadOnly();
     } else {
-      return (
-        <ListItem divider={true} {...this.state.attributes}>
-          <h2>{this.props.children}</h2>
-          <ListItemSecondaryAction>
-            <IconButton onClick={this.openModal} className="btn-flat">
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          </ListItemSecondaryAction>
-
-          <Dialog open={this.state.open}>
-            <DialogTitle id="alert-dialog-title">Umfrage löschen</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Bist du sicher, dass du die Umfrage löschen möchtest?
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.handleClose} color="primary">
-                Abbrechen
-              </Button>
-              <Button
-                onClick={event => this.deletePoll(event)}
-                autoFocus={true}
-              >
-                Ok
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </ListItem>
-      );
+      return this.renderEditMode();
     }
   }
 
-  private openModal = () => this.setState({ open: true });
-  private handleClose = () => this.setState({ open: false });
-  private deletePoll = (event: any) => {
-    onClickDeletePollButton(event, this.state.editor, this.state.parent);
-    this.setState({ open: false });
-  };
+  private renderReadOnly() {
+    return (
+      <ListItem divider={true} {...this.state.attributes}>
+        <h2>{this.props.children}</h2>
+      </ListItem>
+    );
+  }
+
+  private renderEditMode() {
+    return (
+      <ListItem divider={true} {...this.state.attributes}>
+        <h2>{this.props.children}</h2>
+        <ListItemSecondaryAction>
+          <IconButton onClick={this.openDialog} className="btn-flat">
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        </ListItemSecondaryAction>
+
+        <Dialog open={this.state.isDialogOpen}>
+          <DialogTitle id="alert-dialog-title">Umfrage löschen</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Bist du sicher, dass du die Umfrage löschen möchtest?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.closeDialog} color="primary">
+              Abbrechen
+            </Button>
+            <Button onClick={event => this.deletePoll(event)} autoFocus={true}>
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </ListItem>
+    );
+  }
+  private onClickDeletePollButton(event: any, editor: any, node: any) {
+    event.preventDefault();
+    this.deleteNode(editor, node);
+  }
+
+  private deleteNode(editor: any, node: any) {
+    return editor.removeNodeByKey(node.key);
+  }
+
+  private openDialog = () => this.setState({ isDialogOpen: true });
+  private closeDialog = () => this.setState({ isDialogOpen: false });
+  private deletePoll(event: any) {
+    this.onClickDeletePollButton(event, this.state.editor, this.state.parent);
+    this.closeDialog();
+  }
 }
