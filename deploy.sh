cd4524e4be0c05fbaa8bbb4ff50e27d272d3b3b5
Falko Benthin <Falko.Benthin@hpi.de>
2019-01-10 15:45:24 +0100

    achter Deployment-Versuch

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 eval "echo \"$(cat k8s/kube.yaml)\"" > k8s/kube.conf
 export KUBECONFIG=k8s/kube.conf
+cat k8s/kube.conf
 kubectl apply -f "k8s/edtrio-client-deployment.yml"
 # kubectl apply -f "k8s/service.yaml"
 # kubectl apply -f "k8s/ingress.yaml"
