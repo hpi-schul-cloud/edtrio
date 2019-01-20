3163a05d7e04c169aacbd6d1f1b84e9bdf579588
Konstantin Dobler <konstantin.dobler@student.hpi.de>
2019-01-20 22:09:19 +0100

    remove bad design stuff

--- edtrio-client/src/plugins/poll/PollToggles.tsx
+++ edtrio-client/src/plugins/poll/PollToggles.tsx
@@ -7,61 +7,59 @@ import VoteAllowedIcon from "@material-ui/icons/SpeakerNotes";
 import VoteForbiddenIcon from "@material-ui/icons/SpeakerNotesOff";
 import VisibleIcon from "@material-ui/icons/Visibility";
 import InvisibleIcon from "@material-ui/icons/VisibilityOff";
-import { makeStyles } from "@material-ui/styles";
+
 import React from "react";
 import { Block, Editor, Node, Text } from "slate";
 import { PollStateContext } from "../../context/PollStateContext";
 
-const useStyles = makeStyles({
-  root: {
-    color: grey[800],
-    "&$checked": {
-      color: grey[900],
-    },
-  },
-  checked: {},
-});
-
 export default class PollToggles extends React.Component {
   public render() {
-    const classes = useStyles();
-
     return (
       <PollStateContext.Consumer>
         {({ locked, updateLocked, showResults, updateShowResults }) => (
           <Grid container={true} justify="space-evenly" alignItems="center">
-            <FormControlLabel
-              classes={{ label: "pollToggleLabel" }}
-              control={
-                <Checkbox
-                  checked={!locked}
-                  icon={<VoteForbiddenIcon fontSize="large" />}
-                  checkedIcon={<VoteAllowedIcon fontSize="large" />}
-                  onChange={() => updateLocked(!locked)}
-                  value="checkedA"
-                  color="primary"
-                />
-              }
-              label={this.getVoteLabel(locked)}
-            />
-            <FormControlLabel
-              classes={{ label: "pollToggleLabel" }}
-              control={
-                <Checkbox
-                  classes={{
-                    root: classes.root,
-                    checked: classes.checked,
-                  }}
-                  checked={showResults}
-                  icon={<InvisibleIcon fontSize="large" />}
-                  checkedIcon={<VisibleIcon fontSize="large" />}
-                  onChange={() => updateShowResults(!showResults)}
-                  value="checkedB"
-                  color="primary"
-                />
-              }
-              label={this.getResultsLabel(showResults)}
-            />
+            <Grid
+              container={true}
+              xs={true}
+              justify="space-evenly"
+              alignItems="center"
+            >
+              <FormControlLabel
+                classes={{ label: "pollToggleLabel" }}
+                control={
+                  <Checkbox
+                    checked={!locked}
+                    icon={<VoteForbiddenIcon fontSize="large" />}
+                    checkedIcon={<VoteAllowedIcon fontSize="large" />}
+                    onChange={() => updateLocked(!locked)}
+                    value="checkedA"
+                    color="default"
+                  />
+                }
+                label={this.getVoteLabel(locked)}
+              />
+            </Grid>
+            <Grid
+              container={true}
+              xs={true}
+              justify="space-evenly"
+              alignItems="center"
+            >
+              <FormControlLabel
+                classes={{ label: "pollToggleLabel" }}
+                control={
+                  <Checkbox
+                    checked={showResults}
+                    icon={<InvisibleIcon fontSize="large" />}
+                    checkedIcon={<VisibleIcon fontSize="large" />}
+                    onChange={() => updateShowResults(!showResults)}
+                    value="checkedB"
+                    color="default"
+                  />
+                }
+                label={this.getResultsLabel(showResults)}
+              />
+            </Grid>
 
             <br />
           </Grid>
