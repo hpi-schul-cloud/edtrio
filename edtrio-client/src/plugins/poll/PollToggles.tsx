f596e6762fe9d5909c955e9f8fdd283ea77cfbc7
Konstantin Dobler <konstantin.dobler@student.hpi.de>
2019-01-20 21:22:21 +0100

    commit

--- edtrio-client/src/plugins/poll/PollToggles.tsx
+++ edtrio-client/src/plugins/poll/PollToggles.tsx
@@ -1,4 +1,5 @@
 import Checkbox from "@material-ui/core/Checkbox";
+import grey from "@material-ui/core/colors/grey";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormGroup from "@material-ui/core/FormGroup";
 import Grid from "@material-ui/core/Grid";
@@ -6,13 +7,24 @@ import VoteAllowedIcon from "@material-ui/icons/SpeakerNotes";
 import VoteForbiddenIcon from "@material-ui/icons/SpeakerNotesOff";
 import VisibleIcon from "@material-ui/icons/Visibility";
 import InvisibleIcon from "@material-ui/icons/VisibilityOff";
+import { makeStyles } from "@material-ui/styles";
 import React from "react";
 import { Block, Editor, Node, Text } from "slate";
 import { PollStateContext } from "../../context/PollStateContext";
 
+const useStyles = makeStyles({
+  root: {
+    color: grey[800],
+    "&$checked": {
+      color: grey[900],
+    },
+  },
+  checked: {},
+});
+
 export default class PollToggles extends React.Component {
   public render() {
-    const { ...attributes } = this.props;
+    const classes = useStyles();
 
     return (
       <PollStateContext.Consumer>
@@ -36,6 +48,10 @@ export default class PollToggles extends React.Component {
               classes={{ label: "pollToggleLabel" }}
               control={
                 <Checkbox
+                  classes={{
+                    root: classes.root,
+                    checked: classes.checked,
+                  }}
                   checked={showResults}
                   icon={<InvisibleIcon fontSize="large" />}
                   checkedIcon={<VisibleIcon fontSize="large" />}
