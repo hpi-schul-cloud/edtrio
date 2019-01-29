09d3e7e70675b5606e44e7dacd149f128ea1dbd6
Konstantin Dobler <konstantin.dobler@student.hpi.de>
2019-01-29 11:31:00 +0100

    better placeheholder start

--- edtrio-client/src/plugins/poll/Poll.tsx
+++ edtrio-client/src/plugins/poll/Poll.tsx
@@ -5,13 +5,17 @@ import AddIcon from "@material-ui/icons/Add";
 import SendIcon from "@material-ui/icons/Send";
 import { List } from "immutable";
 import React from "react";
-import { Block, Editor, Text } from "slate";
+import { Block, Editor, Node, Text } from "slate";
 import { PollStateContext } from "../../context/PollStateContext";
 import { apolloClient } from "../../EditorWrapper/apolloClient";
 import {
   addSubmissionToPollAnswer,
   addSubmissionToPollAnswerVariables,
 } from "../../graphqlOperations/generated-types/addSubmissionToPollAnswer";
+import {
+  createPoll,
+  createPollVariables,
+} from "../../graphqlOperations/generated-types/createPoll";
 import {
   createPollAnswer,
   createPollAnswerVariables,
@@ -22,6 +26,7 @@ import {
 } from "../../graphqlOperations/generated-types/poll";
 import {
   ADD_SUBMISSION_TO_POLL_ANSWER,
+  CREATE_POLL,
   CREATE_POLL_ANSWER,
   POLL_QUERY,
 } from "../../graphqlOperations/operations";
@@ -33,20 +38,70 @@ import TemplatePicker from "./TemplatePicker";
 import PollTogglesEditMode from "./toggles/PollTogglesEditMode";
 import PollTogglesReadOnlyMode from "./toggles/PollTogglesReadOnlyMode";
 
-export async function createNewPollAnswer(pollId: any, text: any = "") {
+export function createPollAnswerDBEntryAndFetchId(pollId: any) {
+  let Id;
+  apolloClient
+    .mutate<createPollAnswer, createPollAnswerVariables>({
+      mutation: CREATE_POLL_ANSWER,
+      variables: { pollId },
+    })
+    .then(pollAnswer => {
+      // @ts-ignore: I just created it.......... amk
+      Id = pollAnswer.data.createPollAnswer.id;
+    });
+  return Id;
+}
+
+function DBasifyPoll(pollNode: Block) {
+  let pollId;
+  apolloClient
+    .mutate<createPoll, createPollVariables>({
+      mutation: CREATE_POLL,
+      variables: { votingAllowed: false, displayResults: false },
+    })
+    .then(poll => {
+      // @ts-ignore: I just created it.......... amk
+      pollId = poll.data.createPoll.id;
+    });
+
+  const neededDBAnswerEntries = pollNode.nodes.size - 1;
+  const dbEntries = Array<any>();
+  for (let index = 0; index < neededDBAnswerEntries; index++) {
+    dbEntries.push(createPollAnswerDBEntryAndFetchId(pollId));
+  }
+
+  const newPollContent = Array<any>();
+  newPollContent.push(
+    Block.create({
+      type: "poll_question",
+      nodes: List([Text.create(pollNode.nodes[0].text)]),
+    }),
+  );
+  dbEntries.forEach((answerId, idx) => {
+    newPollContent.push(
+      Block.create({
+        type: "poll_question",
+        nodes: List([Text.create(pollNode.nodes[idx + 1].text)]),
+        data: { id: answerId },
+      }),
+    );
+  });
+
+  const newPollNode = Block.create({
+    type: "poll",
+    nodes: List(newPollContent),
+    data: { id: pollId },
+  });
+}
+
+export async function createNewPollAnswer(text: any = "") {
   if (!pollId) {
     return Block.create({
       type: "poll_answer",
       nodes: List([Text.create(text)]),
     });
   }
-  const pollAnswer = await apolloClient.mutate<
-    createPollAnswer,
-    createPollAnswerVariables
-  >({
-    mutation: CREATE_POLL_ANSWER,
-    variables: { pollId },
-  });
+
   if (pollAnswer.data) {
     return Block.create({
       type: "poll_answer",
