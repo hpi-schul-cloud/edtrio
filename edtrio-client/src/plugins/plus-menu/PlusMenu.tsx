e9d53391b031c1c25a929b04aa5995a15282b917
Konstantin Dobler <konstantin.dobler@student.hpi.de>
2019-01-29 16:27:24 +0100

    plus menu should not appearing in poll

--- edtrio-client/src/plugins/plus-menu/PlusMenu.tsx
+++ edtrio-client/src/plugins/plus-menu/PlusMenu.tsx
@@ -88,13 +88,11 @@ class PlusMenu extends PureComponent<IPlusMenuProps, IPlusMenuState> {
       const nodeElBBox = nodeEl.getBoundingClientRect();
       // don't render the menu in the title
       if (
-        ("type" in startBlock &&
-          startBlock.type !== "title" &&
-          startBlock.type !== "multiple-choice-question" &&
-          startBlock.type !== "multiple-choice-answer") ||
-        ("type" in startBlock &&
-          startBlock.type !== "title" &&
-          !startBlock.type.startsWith("poll"))
+        "type" in startBlock &&
+        startBlock.type !== "title" &&
+        startBlock.type !== "multiple-choice-question" &&
+        startBlock.type !== "multiple-choice-answer" &&
+        !startBlock.type.startsWith("poll")
       ) {
         const top = nodeElBBox.top + window.pageYOffset;
 
