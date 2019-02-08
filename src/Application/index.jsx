4ea6c9572fa0743c9629993fad94bf0c4b4deb6b
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-02-08 15:46:31 +0100

    add base url for react router

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter } from "react-router-dom"
 
 import Workspace from "~/workspace"
 import GlobalStyle from "./GlobalStyle"
+import config from "~/config"
 
 const Application = () => {
     return (
-        // TODO set base url for schul-cloud integration
-        <BrowserRouter>
+        <BrowserRouter
+            basename={config.IS_EMBED && `${window.location.pathname}`}>
             <div>
                 <GlobalStyle />
                 <Workspace />
