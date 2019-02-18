50e7cf61e0e3f3f8c0867cb34a745feb7d22b458
Till Prochaska <mail@tillprochaska.de>
2019-02-18 20:04:52 +0100

    Add optional title to Button component

--- src/plugins/sorting-task/Button/index.tsx
+++ src/plugins/sorting-task/Button/index.tsx
@@ -4,16 +4,19 @@ import "./style.scss";
 interface IProps {
   onClick?: () => void,
   theme?: string,
+  title?: string,
+  className?: string,
 }
 
 export default class Button extends React.PureComponent<IProps> {
 
   public render() {
-    const { onClick, theme } = this.props;
+    const { onClick, theme, title, className } = this.props;
 
     return (
       <button
-        className={ `st-button st-button--${theme}` }
+        className={ `st-button st-button--${theme} ${className}` }
+        title={ title }
         onClick={ onClick }
       >
         { this.props.children }
