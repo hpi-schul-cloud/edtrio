0880df08be6aa34e30883d25117e662b6952fa70
Knorrke <knorr.b@gmx.de>
2019-05-10 14:27:01 +0200

    additional helpers for plugins inside row

--- src/plugins/rows/Editor/render.js
+++ src/plugins/rows/Editor/render.js
@@ -11,6 +11,13 @@ export default function({
         renderIntoExtendedSettings,
         PrimarySettingsWrapper,
         insert: options => rows.insert(index + 1, options),
+        replace: options => {
+            rows.remove(index)
+            rows.insert(index, options)
+        },
+        remove: () => {
+            rows.remove(index)
+        },
         mergeWithPrevious: merge => {
             if (index - 1 < 0) return
             const previous = getDocument(store.state, rows()[index - 1].id)
