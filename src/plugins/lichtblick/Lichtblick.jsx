607b78707d693dbe5ac163d78881d5af3888e1fa
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-10-22 15:23:39 +0200

    WIP

--- src/plugins/lichtblick/Lichtblick.jsx
+++ src/plugins/lichtblick/Lichtblick.jsx
@@ -10,20 +10,15 @@ import { createBoard } from "./utils"
 
 const Lichtblick = ({ focused, state }) => {
     useEffect(() => {
-        if (state.id.value) return
-        state.id.set(shortid.generate())
     }, [])
 
     const { store, dispatch } = useContext(LessonContext)
 
     let lichtblickFrame
-    if (state.id.value) {
+    
         lichtblickFrame = (
             <iframe
-                src={`https://nwdl.eu/lichtblick/dist/${
-                    // TODO maybe make this variable?
-                    state.id.value
-                }`}
+                src={`https://nwdl.eu/lichtblick/dist/`}
                 style={{
                     width: "100%",
                     height: 800,
@@ -33,27 +28,9 @@ const Lichtblick = ({ focused, state }) => {
                 data-identifier="iframe-0"
             />
         )
-    }
+    
     return (
         <div>
-            {/* <Flex alignEnd>
-                <Input
-                    size={24}
-                    placeholder="Lichtblick Titel"
-                    style={{ marginBottom: 15, marginRight: 15, width: 150 }}
-                    onChange={state.title.set}
-                    value={state.title.value}
-                    readOnly={!store.editing}
-                />
-                <Input
-                    size={16}
-                    style={{ marginBottom: 15, flexGrow: 1 }}
-                    placeholder="Lichtblick Beschreibung"
-                    onChange={state.description.set}
-                    value={state.description.value}
-                    readOnly={!store.editing}
-                />
-            </Flex> */}
             {lichtblickFrame}
         </div>
     )
