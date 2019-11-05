92ba6f4efecd30eac3f68b287dce9502e235bdb8
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-11-05 17:04:52 +0100

    new dispatch middleware

--- src/Contexts/Lesson.jsx
+++ src/Contexts/Lesson.jsx
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react"
 import qs from "qs"
 import { mergeDiff } from "~/utils/diff"
+import { createDispatch, thunkMiddleware } from "~/utils/dispatch"
 
 const q = qs.parse(window.location.search, { ignoreQueryPrefix: true })
 
@@ -347,10 +348,10 @@ function reducer(state, { type, payload }) {
 
 const LessonContext = React.createContext()
 
-export function LessonContextProvider({ children }) {
+export function LessonContextProvider({ children, dispatchMiddleware = [] }) {
     const [state, dispatch] = useReducer(reducer, initialState)
 
-    const value = { store: state, dispatch }
+    const value = { store: state, dispatch: createDispatch([thunkMiddleware], dispatch, state) }
 
     return (
         <LessonContext.Provider value={value}>
---
+++ src/utils/dispatch.js
@@ -0,0 +1,25 @@
+import { type } from "os"
+
+export const createDispatch = (middlewares = [], dispatch, state) => {
+	if(middlewares === 0){
+		return dispatch
+	} else {
+		middlewares.push((() => action => dispatch(action)))
+		const chain = middlewares
+			.map((a) => a({state, dispatch}))
+			.reduce((a,b) => a(b))
+		return (action) => {
+			return chain(action)
+		}
+	}
+}
+
+export const thunkMiddleware = () => {
+	return ({state, dispatch}) => (next) => action => {
+		if(typeof action === 'function'){
+			return action({state, dispatch})
+		}
+
+		return next(action)
+	}
+}
