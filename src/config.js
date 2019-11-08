254a3f674dda789203b0b4d76048694dd8104d0a
CeEv <cedric@it-4d.de>
2019-11-08 14:33:40 +0100

    fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    EDITOR_API_URL: process.env.EDITOR_SOCKET_URL || "http://localhost:4001",
+    EDITOR_API_URL: process.env.EDITOR_API_URL || "http://localhost:4001",
     SERVER_API_URL: process.env.SERVER_API_URL || "http://localhost:3030",
    // DISABLE_BACKEND: true,
     EDITOR_SOCKET_URL: process.env.EDITOR_SOCKET_URL || "ws://api.edtr.l"
