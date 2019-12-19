d001afecf6fe971fb2dc5bc5145c74ea854950d4
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-12-19 14:51:42 +0100

    hide controlls

--- src/workspace/Lesson/SectionOverview/Settings/index.jsx
+++ src/workspace/Lesson/SectionOverview/Settings/index.jsx
@@ -83,9 +83,11 @@ const Settings = () => {
 		<Portal>
 			<Wrapper justifyBetween>
 				<Flex noWrap>
+					{/*
 					<Icon src={duplicateIcon} />
 					<Icon src={shareIcon} />
 					<Icon src={infoIcon} />
+					*/}
 					<DeleteModal
 						sectionTitle={
 							activeSection.title ||
@@ -102,7 +104,7 @@ const Settings = () => {
 								/>
 							)
 						}}
-					/> { /* 
+					/>{/* // show / hide section
 					<Icon
 						src={
 							activeSection.visible ? previewIcon : noPreviewIcon
