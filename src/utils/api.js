f9979c2fcabb091459d8cb8f86b0a5ba4794c183
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-02-11 11:29:34 +0100

    linting

--- src/utils/api.js
+++ src/utils/api.js
@@ -39,160 +39,161 @@ const SERVER_API_URL = config.SERVER_API_URL || ""
 const EDITOR_API_URL = config.EDITOR_API_URL || ""
 
 const bodyRequest = (
-    type,
-    baseURL,
-    endpoint,
-    body,
-    files,
-    uploadProgress,
-    fakeResponse,
+	type,
+	baseURL,
+	endpoint,
+	body,
+	files,
+	uploadProgress,
+	fakeResponse,
 ) => {
-    if (fakeResponse && config.DISABLE_BACKEND) {
-        return new Promise(resolve =>
-            setTimeout(() => resolve(fakeResponse), 250),
-        )
-    }
+	if (fakeResponse && config.DISABLE_BACKEND) {
+		return new Promise(resolve =>
+			setTimeout(() => resolve(fakeResponse), 250),
+		)
+	}
 
-    return new Promise((resolve, reject) => {
-        let data
+	return new Promise((resolve, reject) => {
+		let data
 
-        if (files) {
-            data = new FormData()
+		if (files) {
+			data = new FormData()
 
-            Object.keys(body).forEach(key => {
-                data.append(key, body[key])
-            })
+			Object.keys(body).forEach(key => {
+				data.append(key, body[key])
+			})
 
-            Object.keys(files).forEach(key => {
-                data.append(key, files[key])
-            })
-        } else {
-            data = body
-        }
+			Object.keys(files).forEach(key => {
+				data.append(key, files[key])
+			})
+		} else {
+			data = body
+		}
 
-        axios[type](endpoint, data, {
-            baseURL, // TODO include test system? staging?‚
-            onUploadProgress:
+		axios[type](endpoint, data, {
+			baseURL, // TODO include test system? staging?‚
+			onUploadProgress:
                 uploadProgress && typeof uploadProgress === "function"
-                    ? uploadProgress
-                    : undefined,
-        })
-            .then(result => {
-                resolve(result.data)
-            })
-            .catch(err => {
-                if (
-                    err &&
+                	? uploadProgress
+                	: undefined,
+		})
+			.then(result => {
+				resolve(result.data)
+			})
+			.catch(err => {
+				if (
+					err &&
                     err.response &&
                     err.response.data &&
                     err.response.data.error
-                ) {
-                    err.description = err.response.data.error
-                    reject(err)
-                } else {
-                    reject(err)
-                }
-            })
-    })
+				) {
+					err.description = err.response.data.error
+					reject(err)
+				} else {
+					reject(err)
+				}
+			})
+	})
 }
 
 class Api {
 
-    constructor( baseURL ) {
-        this.baseURL = baseURL
-    }
+	constructor( baseURL ) {
+		this.baseURL = baseURL
+	}
 
-    get (endpoint, fakeResponse) {
-        if (fakeResponse && config.DISABLE_BACKEND) {
-            return new Promise(resolve =>
-                setTimeout(() => resolve(fakeResponse), 250),
-            )
-        }
+	get (endpoint, fakeResponse) {
+		console.log(endpoint)
+		if (fakeResponse && config.DISABLE_BACKEND) {
+			return new Promise(resolve =>
+				setTimeout(() => resolve(fakeResponse), 250),
+			)
+		}
 
-        return new Promise((resolve, reject) => {
-            axios
-                .get(endpoint, {
-                    baseURL: this.baseURL,
-                })
-                .then(result => resolve(result.data))
-                .catch(err => {
-                    if (
-                        err &&
+		return new Promise((resolve, reject) => {
+			axios
+				.get(endpoint, {
+					baseURL: this.baseURL,
+				})
+				.then(result => resolve(result.data))
+				.catch(err => {
+					if (
+						err &&
                         err.response &&
                         err.response.data &&
                         err.response.data.error
-                    ) {
-                        err.description = err.response.data.error
-                        reject(err)
-                    } else {
-                        reject(err)
-                    }
-                })
-        })
-    }
+					) {
+						err.description = err.response.data.error
+						reject(err)
+					} else {
+						reject(err)
+					}
+				})
+		})
+	}
 
-    post (endpoint, body, files, uploadProgress, fakeResponse) {
-        return bodyRequest(
-            "post",
-            this.baseURL,
-            endpoint,
-            body,
-            files,
-            uploadProgress,
-            fakeResponse,
-        )
-    }
+	post (endpoint, body, files, uploadProgress, fakeResponse) {
+		return bodyRequest(
+			"post",
+			this.baseURL,
+			endpoint,
+			body,
+			files,
+			uploadProgress,
+			fakeResponse,
+		)
+	}
 
-    put (endpoint, body, files, uploadProgress, fakeResponse) {
-        return bodyRequest("put", this.baseURL, endpoint, body, files, uploadProgress, fakeResponse)
-    }
+	put (endpoint, body, files, uploadProgress, fakeResponse) {
+		return bodyRequest("put", this.baseURL, endpoint, body, files, uploadProgress, fakeResponse)
+	}
 
-    patch (endpoint, body, files, uploadProgress, fakeResponse){
-        return bodyRequest(
-            "patch",
-            this.baseURL,
-            endpoint,
-            body,
-            files,
-            uploadProgress,
-            fakeResponse,
-        )
-    }
+	patch (endpoint, body, files, uploadProgress, fakeResponse){
+		return bodyRequest(
+			"patch",
+			this.baseURL,
+			endpoint,
+			body,
+			files,
+			uploadProgress,
+			fakeResponse,
+		)
+	}
 
-    delete (endpoint, fakeResponse) {
-        if (fakeResponse && process.env.NODE_ENV !== "production") {
-            return new Promise(resolve =>
-                setTimeout(() => resolve(fakeResponse), 250),
-            )
-        }
+	delete (endpoint, fakeResponse) {
+		if (fakeResponse && process.env.NODE_ENV !== "production") {
+			return new Promise(resolve =>
+				setTimeout(() => resolve(fakeResponse), 250),
+			)
+		}
 
-        return new Promise((resolve, reject) => {
-            axios
-                .delete(endpoint, {
-                    baseURL: this.baseURL, // TODO include test system? staging?
-                    headers: {
-                        Authorization:
+		return new Promise((resolve, reject) => {
+			axios
+				.delete(endpoint, {
+					baseURL: this.baseURL, // TODO include test system? staging?
+					headers: {
+						Authorization:
                             (getCookie("jwt").startsWith("Bearer ")
-                                ? ""
-                                : "Bearer ") + getCookie("jwt"),
-                    },
-                })
-                .then(result => resolve(result.data))
-                .catch(err => {
-                    if (
-                        err &&
+                            	? ""
+                            	: "Bearer ") + getCookie("jwt"),
+					},
+				})
+				.then(result => resolve(result.data))
+				.catch(err => {
+					if (
+						err &&
                         err.response &&
                         err.response.data &&
                         err.response.data.error
-                    ) {
-                        err.description = err.response.data.error
-                        reject(err)
-                    } else {
-                        reject(err)
-                    }
-                })
-        })
-    }
+					) {
+						err.description = err.response.data.error
+						reject(err)
+					} else {
+						reject(err)
+					}
+				})
+		})
+	}
 }
 
 export default Api
