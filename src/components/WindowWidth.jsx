f641377503a53dfa733246265bfede8ef0a2b370
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-02-19 15:24:31 +0100

    refactor windowwidth component

--- src/components/WindowWidth.jsx
+++ src/components/WindowWidth.jsx
@@ -5,30 +5,30 @@ import config from "~/config"
 
 
 
-const onWidthChange = (setState) => () => {
-	setState({width: window.innerWidth});
-}
-
-// const [width, setWidth]  = useState(viewportWidth());
-// window.addEventListener("resize", onWidthChange(setWidth))
 
 class WindowWidth extends React.Component {
 
 	constructor(props) {
 		super(props);
-  	this.state = {width: window.innerWidth};
-  	window.addEventListener("resize", onWidthChange(this.setState))
-
-		this.mofidifiedChildren =
-			React.Children.map(props.children,child =>
-				React.cloneElement(child, {windowWidth: this.state.width} ))
+		this.state = {width: window.innerWidth};
 	}
 
+	componentDidMount() {
+		window.addEventListener("resize", () => this.setState({width: window.innerWidth}));
+	}
+
+	renderChildren() {
+		return React.Children.map(this.props.children,child =>
+			React.cloneElement(child, {windowWidth: this.state.width} ))
+	}
+
+
 	render() {
-  	return (
-  		<React.Fragment>
-  			{this.mofidifiedChildren}
-  		</React.Fragment>)
+		return (
+			<React.Fragment>
+				{this.renderChildren()}
+			</React.Fragment>
+		)
 	}
 }
 
