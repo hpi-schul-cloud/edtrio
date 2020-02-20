1a8a0a0b245248d77c74e5d71ad03d3056744988
Luisa <luisa.schrott@gmail.com>
2020-02-20 10:22:12 +0100

    added hight to icon

--- src/components/TaskIcon.jsx
+++ src/components/TaskIcon.jsx
@@ -1,73 +1,50 @@
-import React, { useState } from "react"
-import styled from "styled-components"
-import config from "~/config"
+import React, { useState } from "react";
+import styled from "styled-components";
+import config from "~/config";
 
-import Icon from "~/assets/task-icon.svg"
-import WindowWidth from "./WindowWidth"
-
+import Icon from "~/assets/task-icon.svg";
+import WindowWidth from "./WindowWidth";
 
 const TaskBackground = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-width: 40px;
-height: 40px;
-border-radius: 50%;
-background-color: ${props => props.BackgroundColor};
-margin-right:	0.694rem;
-flex-shrink: 0;
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 2.5rem;
+	height: 2.5rem;
+	border-radius: 50%;
+	background-color: ${(props) => props.BackgroundColor};
+	margin-right: 0.694rem;
+	flex-shrink: 0;
+`;
 
-const IconWhite =styled(Icon)`
-color: white;
-`
-const IconColor =styled(Icon)`
-color:  ${props => props.BackgroundColor};
-margin-right: 0.694rem;
-font-size: 12px;
-flex-shrink: 0;
+const IconWhite = styled(Icon)`
+	color: white;
+`;
+const IconColor = styled(Icon)`
+	color: ${(props) => props.Color};
+	margin-right: 0.694rem;
+	flex-shrink: 0;
+	margin-top: 0.2rem;
+`;
 
-`
-// const viewportWidth = () =>{
-// 	return window.innerWidth;
-// }
-// const onWidthChange = (setWidth) => () => {
-// 	setWidth(viewportWidth());
-// }
-
-
-const TaskIcon = ({ color = "#FFF", windowWidth}) => {
-
-	// const [width, setWidth]  = useState(viewportWidth());
-	// window.addEventListener("resize", onWidthChange(setWidth))
-	console.log(windowWidth)
-
-	if (windowWidth > config.breakpoints.tablet ){
+const TaskIcon = ({ color = "#FFF", windowWidth }) => {
+	if (windowWidth > config.breakpoints.tablet) {
 		return (
-			<TaskBackground BackgroundColor={color} >
-				<IconWhite />
+			<TaskBackground BackgroundColor={color}>
+				<IconWhite height="1rem" />
 			</TaskBackground>
-		)
+		);
+	} else {
+		return <IconColor height="0.8rem" Color={color} />;
 	}
-	else {
-		return (
-			<IconColor BackgroundColor={color}/>
-		)
-	}
+};
 
-}
-
-
-const ResponsivTaskIcon =  ({ color = "#FFF"}) =>{
+const ResponsivTaskIcon = ({ color = "#FFF" }) => {
 	return (
 		<WindowWidth>
 			<TaskIcon color={color} />
 		</WindowWidth>
-	)
-} 
-
-
-
-
+	);
+};
 
 export default ResponsivTaskIcon;
