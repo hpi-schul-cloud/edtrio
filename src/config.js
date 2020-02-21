09ea7898ea9d8caa800682906d159fece04143f2
Luisa <luisa.schrott@gmail.com>
2020-02-21 16:47:48 +0100

    added breakpoints

--- src/config.js
+++ src/config.js
@@ -1,9 +1,14 @@
 const config = {
-    EDITOR_API_URL: process.env.EDITOR_API_URL || "http://localhost:4001",
-    SERVER_API_URL: process.env.SERVER_API_URL || "http://localhost:3030",
-    EDITOR_SOCKET_URL: process.env.EDITOR_SOCKET_URL || "ws://localhost:4001",
+	EDITOR_API_URL: process.env.EDITOR_API_URL || "http://localhost:4001",
+	SERVER_API_URL: process.env.SERVER_API_URL || "http://localhost:3030",
+	EDITOR_SOCKET_URL: process.env.EDITOR_SOCKET_URL || "ws://localhost:4001",
 
-}
+	breakpoints: {
+		tablet: 750,
+		desktop: 991,
+		desktopLg: 1200,
+	},
+};
 // eslint-disable-next-line no-console
-console.info('config', config)
-export default config
+console.info("config", config);
+export default config;
