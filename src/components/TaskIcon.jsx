b892b3e1da81aa42614145c14c207a86d1041527
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2020-02-24 11:32:15 +0100

    Update TaskIcon.jsx

--- src/components/TaskIcon.jsx
+++ src/components/TaskIcon.jsx
@@ -5,15 +5,15 @@ import Icon from "~/assets/task-icon.svg"
 
 
 const TaskBackground = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-width: 40px;
-height: 40px;
-border-radius: 50%;
-background: linear-gradient(45deg, #75270D 0%, #B6916A 100%);
-margin-right	10px;
-flex-shrink: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 40px;
+	height: 40px;
+	border-radius: 50%;
+	background: linear-gradient(45deg, #75270D 0%, #B6916A 100%);
+	margin-right	10px;
+	flex-shrink: 0;
 `
 
 
