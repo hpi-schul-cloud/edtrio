a47d8259251ca5aaa17a7435bf0a188c8d95cbf8
CeEv <cedric@it-4d.de>
2020-02-25 14:29:46 +0100

    Add plugin menüs.

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,6 +8,24 @@ import {
 	ChildStateTypeConfig,
 } from '@edtr-io/plugin';
 
+import {
+	createIcon,
+	faAnchor,
+	faCaretSquareDown,
+	faCode,
+	faCubes,
+	faDotCircle,
+	faFileAlt,
+	faFilm,
+	faImages,
+	faKeyboard,
+	faNewspaper,
+	faParagraph,
+	faPhotoVideo,
+	faQuoteRight
+} from '@edtr-io/ui'
+  
+
 import { createRowsPlugin } from "@edtr-io/plugin-rows"
 
 import { createAnchorPlugin } from "@edtr-io/plugin-anchor"
@@ -101,7 +119,78 @@ const imagePlugin = createImagePlugin({
 
 export const plugins = {
 	rows: createRowsPlugin({
-		content: { plugin: "text" }
+		content: { plugin: "text" },
+		plugins: [
+			{
+			  name: 'anchor',
+			  title: 'Anchor',
+			  icon: createIcon(faAnchor)
+			},
+			{
+			  name: 'blockquote',
+			  title: 'Blockquote',
+			  icon: createIcon(faQuoteRight)
+			},
+			{
+			  name: 'files',
+			  title: 'Files',
+			  icon: createIcon(faFileAlt)
+			},
+			{
+			  name: 'geogebra',
+			  title: 'GeoGebra',
+			  icon: createIcon(faCubes)
+			},
+/* 			{
+			  name: 'highlight',
+			  title: 'Code Highlight',
+			  icon: createIcon(faCode)
+			}, */
+			{
+			  name: 'image',
+			  title: 'Image',
+			  icon: createIcon(faImages)
+			},
+			{
+			  name: 'inputExercise',
+			  title: 'Input Exercise',
+			  icon: createIcon(faKeyboard)
+			},
+			{
+			  name: 'multimediaExplanation',
+			  title: 'Multimedia Explanation',
+			  icon: createIcon(faPhotoVideo)
+			},
+			{
+			  name: 'scMcExercise',
+			  title: 'Choice Exercise',
+			  icon: createIcon(faDotCircle)
+			},
+/* 			{
+			  name: 'serloInjection',
+			  title: 'Serlo Content',
+			  icon: createIcon(faNewspaper)
+			}, */
+			{
+			  name: 'spoiler',
+			  title: 'Spoiler',
+			  icon: createIcon(faCaretSquareDown)
+			},
+/* 			{
+			  name: 'table',
+			  title: 'Markdown Table'
+			}, */
+			{
+			  name: 'text',
+			  title: 'Text',
+			  icon: createIcon(faParagraph)
+			},
+			{
+			  name: 'video',
+			  title: 'Video',
+			  icon: createIcon(faFilm)
+			}
+		  ]
 	}),
 	text: createTextPlugin({
 		// context: { plugin: "text" }
