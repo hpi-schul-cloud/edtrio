9618a92415dd1d02a212fa4f567dfae93e6ea303
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-03-02 14:52:31 +0100

    load envs from window object
    https://blog.codecentric.de/en/2018/12/react-application-container-environment-aware-kubernetes-deployment/

--- src/config.js
+++ src/config.js
@@ -1,9 +1,18 @@
-const config = {
-	EDITOR_API_URL: process.env.EDITOR_API_URL || "http://localhost:4001",
-	SERVER_API_URL: process.env.SERVER_API_URL || "http://localhost:3030",
-	EDITOR_SOCKET_URL: process.env.EDITOR_SOCKET_URL || "ws://localhost:4001",
-	HOMEWORK_URI: process.env.HOMEWORK_URI || "/homework",
+const getEnv = (name) => {
+	// env variables will be set tot he window object in public/env.js during build
+	// https://blog.codecentric.de/en/2018/12/react-application-container-environment-aware-kubernetes-deployment/
+	// only in development mode you can dynamicly overwrite them using process.env
+	if (process.env.NODE_ENV === 'development') {
+		return process.env[name] || window[name]
+	}
+	return window[name]
+}
 
+const config = {
+	EDITOR_API_URL: getEnv("EDITOR_API_URL"),
+	SERVER_API_URL: getEnv("SERVER_API_URL"),
+	EDITOR_SOCKET_URL: getEnv("EDITOR_SOCKET_URL"),
+	HOMEWORK_URI: getEnv("HOMEWORK_URI"),
 
 	breakpoints: {
 		tablet: 750,
---
+++ src/static/env.js
@@ -0,0 +1,4 @@
+window.EDITOR_API_URL = "http://localhost:4001";
+window.SERVER_API_URL = "http://localhost:3030";
+window.EDITOR_SOCKET_URL = "ws://localhost:4001";
+window.HOMEWORK_URI = "/homework";
