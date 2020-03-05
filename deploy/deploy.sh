28061cce3655645c2aedbef7661071a60822b11a
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-03-05 11:14:26 +0100

    update deployment script with @falkobenthin
    @falkobenthin will create the related services for all systems together with @cordlesswool

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -31,19 +31,7 @@ deploy(){
 	DOCKER_TAG=$3 # version/tag of the image to use. Usually the branch name or a GIT_SHA
 	DOCKER_SERVICE_NAME=$4 # docker service name on server
 
-	COMPOSE_SRC=$5 # name of the docker-compose file which should be used as.
-	COMPOSE_TARGET=$6 # name as which the compose file should be pushed to the server (auto prefixed with "docker-compose-")
-	STACK_NAME=$7 # swarm stack name
-
 	echo "deploy " $DOCKER_IMAGE ":" $DOCKER_TAG " to " $SYSTEM " as " $DOCKER_SERVICE_NAME
-	echo "COMPOSEFILE: " $COMPOSE_SRC " => " $COMPOSE_TARGET
-
-	# generate new compose file
-	eval "echo \"$( cat $COMPOSE_SRC )\"" > docker-compose-$COMPOSE_TARGET
-
-	# deploy new compose file
-	scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa docker-compose-$COMPOSE_TARGET linux@$SYSTEM.schul-cloud.org:~
-	ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@$SYSTEM.schul-cloud.org /usr/bin/docker stack deploy -c /home/linux/docker-compose-$COMPOSE_TARGET $STACK_NAME
 
 	# deploy new dockerfile
 	ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i travis_rsa linux@$SYSTEM.schul-cloud.org /usr/bin/docker service update --force --image schulcloud/$DOCKER_IMAGE:$DOCKER_TAG $DOCKER_SERVICE_NAME
@@ -77,11 +65,11 @@ case "$TRAVIS_BRANCH" in
 	develop)
 		echo "develop"
 		# deploy $SYSTEM $DOCKERFILE $DOCKERTAG $DOCKER_SERVICENAME $COMPOSE_DUMMY $COMPOSE_FILE $COMPOSE_SERVICENAME
-		deploy "test" "edtrio" $DOCKERTAG "test-schul-cloud_edtrio" "compose-edtrio_default.dummy" "edtrio.yml" "test-schul-cloud"
+		deploy "test" "edtrio" $DOCKERTAG "test-schul-cloud_edtrio"
 	;;
 	release* | hotfix*)
 		echo "release/hotfix"
-		deploy "staging" "edtrio" $DOCKERTAG "staging_edtrio" "compose-edtrio_default.dummy" "edtrio_default.yml" "staging"
+		deploy "staging" "edtrio" $DOCKERTAG "staging_edtrio"
 	;;
 esac
 
